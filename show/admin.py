250df5f2dee270f4f4f6f1a4e69307dd28cb4db1
zzde <zhangxh1997@gmail.com>
2018-05-22 15:32:53 +0800

    fix admin

--- show/admin.py
+++ show/admin.py
@@ -10,8 +10,26 @@ class UserFilesAdmin(admin.ModelAdmin):
     list_filter = ('is_ective', 'last_submit_time')
     readonly_fields = ('up_count','submit_count')
 
-
 admin.site.register(UserFiles, UserFilesAdmin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 admin.site.site_header = '人机交互项目项目管理中心'
 admin.site.site_title = '人机交互项目项目管理中心'
